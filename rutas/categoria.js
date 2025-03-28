5de83fd000121b14ba1db9dae48eaf7e494fe274
johan pinto <johanpinto232@gmail.com>
2025-03-28 11:42:57 -0500

    module_ignore

--- rutas/categoria.js
+++ rutas/categoria.js
@@ -14,5 +14,21 @@ class categoria {
       throw new Error("error al obtener categorias");
     }
   }
+  async create() {
+    try {
+      const [result] = await conetion.query(
+        "INSERT INT categorias(nombre,descripcion)VALUES(?,?)",
+        [this.nombre, this.descripcion]
+      );
+
+      return {
+        id: result.id,
+        nombre: this.nombre,
+        descripcion: this.descripcion,
+      };
+    } catch (error) {
+      throw new Error("error al crear categorias");
+    }
+  }
 }
 export default categoria;
